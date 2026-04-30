2cc55e8e78018ca5417b5e97efdfc27cf1347ca6
Giani <gianiarip2002@hotmail.com>
2026-04-29 23:00:30 -0400

    up

--- components/gallery.tsx
+++ components/gallery.tsx
@@ -9,7 +9,7 @@ const FEATURED_PRINTS = [
   { url: "/img/maingallery/img1.webp", x: "-18%", y: "-15%", rotate: -4, size: "w-48 md:w-72" },
   { url: "/img/maingallery/img2.webp", x: "22%", y: "10%", rotate: 6, size: "w-56 md:w-80" },
   { url: "/img/maingallery/img3.webp", x: "-28%", y: "35%", rotate: -8, size: "w-40 md:w-64" },
-  { url: "/img/maingallery/img4.webp", x: "12%", y: "45%", rotate: 2, size: "w-64 md:w-96" },
+  { url: "/img/maingallery/img4.webp", x: "12%", y: "45%", rotate: -4, size: "w-64 md:w-96" },
 ];
 
 export function Gallery() {
